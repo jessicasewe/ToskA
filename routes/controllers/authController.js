a7c776e980d9735859521927d8b074058c02f4c2
jessicasewe <jessicasewe89@gmail.com>
2024-05-17 10:26:17 +0000

    authController created

---
+++ routes/controllers/authController.js
@@ -0,0 +1,8 @@
+//handling auth routes
+exports.registerUser = async (req, res) => {
+    res.send('Register user');
+};
+
+exports.loginUser = async (req, res) => {
+    res.send('Login user');
+};
