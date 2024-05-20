df2c129cc4001ae3e8430d96171f484c2dfecae4
jessicasewe <jessicasewe89@gmail.com>
2024-05-20 11:51:27 +0000

    get token for spotify api

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,3 +1,24 @@
-// exports.getMusic = (req, res) => {
-//     res.send('Get music');
-// };
+const axios = require('axios');
+
+const getToken = async () => {
+    try {
+        const clientId = process.env.CLIENT_ID;
+        const clientSecret = process.env.CLIENT_SECRET;
+
+        const authString = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+        const response = await axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', 
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${authString}`
+            },
+        });
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error getting token:', error);
+        throw new Error('Error getting token');
+    }
+}
+
+module.exports = getToken;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
