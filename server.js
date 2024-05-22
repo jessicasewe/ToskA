7449cd4e5f7609dae5395aae9828fbe9938a61d8
jessicasewe <jessicasewe89@gmail.com>
2024-05-22 11:51:38 +0000

    tailwind with ejs

--- server.js
+++ server.js
@@ -11,11 +11,28 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api/music', musicRoutes);
 app.use('/api', movieRoutes);
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Render the register page
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+//serve static files
+app.use(express.static('public'));
+
 
 // Database Connection
 connectDB();
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./views/**/*.ejs',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
