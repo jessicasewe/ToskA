77458d089ee9a081cebb5a496f07901130e202fe
jessicasewe <jessicasewe89@gmail.com>
2024-05-25 22:38:05 +0000

    password encryption

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
   name: {
@@ -20,6 +21,30 @@ const UserSchema = new Schema({
     default: Date.now,
   },
 });
+
+
+// Pre-save hook to hash the password before saving the user
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password using the salt
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords during login
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('user', UserSchema);
 module.exports = User;
